23da77cbae7ef9158cb3680c2fc6872e429805cd
익호 <dldlrgh28@gmail.com>
2024-10-03 22:13:27 +0900

    Initial Commit

---
+++ add.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+num1 = int(input())
+num2 = int(input())
+
+print(f"덧셈결과 :{ add(num1, num2) }" )
+print(f"뺄셈결과 :{ sub(num1, num2) }" )
