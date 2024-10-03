51991ea7b72121f87d12155faffebf5b19b6df5f
익호 <dldlrgh28@gmail.com>
2024-10-03 22:31:03 +0900

    기능추가

--- add.py
+++ add.py
@@ -9,3 +9,15 @@ num2 = int(input())
 
 print(f"덧셈결과 :{ add(num1, num2) }" )
 print(f"뺄셈결과 :{ sub(num1, num2) }" )
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide"
+    
+print(f"곱셈결과 :{ mul(num1, num2) }" )
+print(f"나눗셈결과 :{ div(num1, num2) }" )
